d88385db07dfb3418bf51901ec1250cb6dfbce8b
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-07-03 17:32:40 -0400

    extract url path

--- app/main.js
+++ app/main.js
@@ -5,7 +5,17 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
+    if (request.startsWith("GET /")) {
+      const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(httpResponse);
+    } else {
+      const httpResponse = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+      socket.write(httpResponse);
+    }
+    socket.end();
+  });
   socket.on("close", () => {
     socket.end();
     server.close();
