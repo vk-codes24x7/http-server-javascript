39b148c335566682a108b5f003574b3d64dd4a1c
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-07-03 22:19:18 -0400

    added the correct logic for respond with body

--- app/main.js
+++ app/main.js
@@ -7,17 +7,18 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const stringData = data.toString();
-    const requestPath = stringData.split(" ")[1];
-    console.log(requestPath);
-    if (stringData.startsWith("GET /")) {
-      const responseStatus = requestPath === "/" ? "200 OK" : "404 Not Found";
-      console.log(responseStatus);
-      socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
-    } else if (stringData.includes("/echo/")) {
+    const url = stringData.split(" ")[1];
+    if (url === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (url.includes("/echo/")) {
       const content = stringData.split("/echo/")[1];
-      const responseStatus = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${content}`;
-      socket.write(responseStatus);
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+
     socket.end();
   });
   socket.on("close", () => {
