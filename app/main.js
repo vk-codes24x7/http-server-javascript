9018859c3efad9ccc055016d3027dec38eb2f640
vk-codes24x7 <vinayakrishna1001@gmail.com>
2024-07-05 18:48:11 -0400

    read header

--- app/main.js
+++ app/main.js
@@ -7,13 +7,22 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const stringData = data.toString();
+    console.log(`string data - ${stringData}`);
     const url = stringData.split(" ")[1];
+    console.log(`url - ${url}`);
     if (url === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (url.includes("/echo/")) {
       const content = url.split("/echo/")[1];
+      console.log(`content - ${content}`);
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
+    } else if (url.includes("User-Agent:")) {
+      const userAgent = url.split("User-Agent:")[1];
+      console.log(`user Agent string - ${userAgent}`);
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
